79b7655cbcff0558049963f65f3a3bf49039705c
TheWisePigeon <zozozozeph@gmail.com>
2022-02-08 14:57:45 +0000

    implemented login 1

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const bodyparser = require('body-parser')
+const crypto = require('crypto')
 let constants = require('./const')
 const app = express()
 app.use(bodyparser.urlencoded({ extended : true}))
@@ -8,26 +9,41 @@ const port = process.env.PORT || 3000
 
 app.get('/', (req, res)=>{
     res.sendFile(__dirname + "/home.html")
-    console.log(`Total supply remaining is ${constants.totalSupply}`);
 })
+.post('/', (req, res)=>{
+    let result = req.body
+    let hash = crypto.createHmac('sha256', result.name).digest('hex')
+    console.log(hash);
+    let user = {
+        "name" : result.name,
+        "email" : result.mail,
+        "password" : result.pwd,
+        "balance" : 2000,
+        "ref" : `${hash}`
+    }
+    constants.totalSupply-=2000
+    constants.users.unshift(user)
+    res.send(`Post received <br/><a href="/">Go back to home</a>`)
+})
+
 
 app.get('/index', (req, res)=>{
     res.sendFile(__dirname + "/index.html")
 })
 
-app.post('/', (req, res)=>{
-    let result = req.body
-    let user = {
-        "nom" : result.name,
-        "email" : result.mail,
-        "password" : result.pwd,
-        "balance" : 2000,
-        "ref" : ``
-    }
-    constants.totalSupply-=2000
-    constants.users.unshift(user)
-    res.send(`Post received <br/>You have ${constants.users.length} users registred<br/>`)
+app.get('/login', (req, res)=>{
+    res.sendFile(__dirname + "/login.html")
 })
+.post('/login', (req, res)=>{
+    let email = req.body.mail
+    let pwd = req.body.pwd
+    constants.users.forEach(element => {
+        if (element.email==email && element.password==pwd) {
+            res.send("Match")
+        }
+    });
+})
+
 
 
 
